3a573b3958f892da7a9da5aadb44fb162a29b84a
Pétur Þór Haraldsson <peturh@islandspostur.is>
2025-09-17 13:08:54 +0000

    Remove redundant XML declaration from Openbox configuration

--- modules/openbox/config.nix
+++ modules/openbox/config.nix
@@ -78,8 +78,6 @@ environment.etc."xdg/openbox/menu.xml".text = ''
     </item>
   </menu>
 </openbox_menu>
-''; # Openbox configuration environment.etc."xdg/openbox/rc.xml".text = ''
-<?xml version="1.0" encoding="UTF-8"?>
 <openbox_config
   xmlns="http://openbox.org/"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
