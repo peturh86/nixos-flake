fc65fa4352a42b7acd25afa9ec7ed9fe8bca150f
Pétur Þór Haraldsson <peturh@islandspostur.is>
2025-09-17 10:39:45 +0000

    Remove old kiosk modules directory to resolve conflicts with new structure

--- modules/kiosk/autostart/tint2.nix
+++
@@ -1,8 +0,0 @@
-{ lib, pkgs, ... }:
-{
-  services.kiosk.autostart.lines = lib.mkAfter [
-    "echo \"$(date): About to start tint2\" >> /tmp/openbox-autostart.log"
-    "XDG_CONFIG_DIRS=/etc/xdg ${pkgs.tint2}/bin/tint2 >> /tmp/tint2.log 2>&1 &"
-    "echo \"$(date): tint2 command executed\" >> /tmp/openbox-autostart.log"
-  ];
-}
--- modules/kiosk/system/boot.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, ... }:
-
-with lib;
-
-{
-  # Boot loader
-  boot.loader.systemd-boot.enable = mkDefault true;
-  boot.loader.efi.canTouchEfiVariables = mkDefault true;
-}
